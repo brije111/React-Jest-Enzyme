f24c9384059c63ca7bf061146fcd92b48d19f1b2
Brijesh Kumar <BrijeshKum@hcl.com>
2019-09-30 18:49:32 +0530

    EOD commit

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
-import Footer from '../component/Footer
-import Counter from '../component/counter/Counter';
+import Footer from '../component/Footer;
+//import Counter from '../component/counter/Counter';
 
 it('should render App with child Counter and Footer', ()=>{
     const wrapper = shallow(<App />);
--- src/__tests__/Login.test.tsx
+++ src/__tests__/Login.test.tsx
@@ -2,22 +2,35 @@ import React from 'react';
 import Login from '../component/login/Login';
 import * as enzyme from 'enzyme';
 
-it('should render Login', ()=> {
-    enzyme.shallow(<Login />);
+describe('<Login />', () => {
+    
+    let wrapper: enzyme.ShallowWrapper;
+    const setState = jest.fn();
+    const useStateSpy = jest.spyOn(React, 'useState');
+    useStateSpy.mockImplementation(init => [init, setState]);
+
+    beforeEach(() => {
+        wrapper = enzyme.shallow(<Login />);
+    });
+
+    it('should show login success', () => {
+        //const wrapper = enzyme.shallow(<Login />);
+
+        //expect(wrapper.find('Test')).toMatchSnapshot();
+
+        const usernameInput = wrapper.find('input.input1');
+        usernameInput.simulate('change', { target: { value: 'admin@123' } });
+
+        const passwordInput = wrapper.find('input.input2');
+        passwordInput.simulate('change', { target: { value: 'admin123' } });
+
+        wrapper.find('button').simulate('click');
+
+        expect(wrapper.state('isLogined')).toBe(true);
+    })
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
 })
 
-it('should show login success', ()=>{
-    const wrapper = enzyme.shallow(<Login />);
-
-    //expect(wrapper.find('Test')).toMatchSnapshot();
-
-    const usernameInput = wrapper.find('input.input1');
-    usernameInput.simulate('change', { target: { value: 'admin@123' } });
-
-    const passwordInput = wrapper.find('input.input2');
-    passwordInput.simulate('change', { target: { value: 'admin123' } });
-
-    wrapper.find('button').simulate('click');
-
-    expect(wrapper.state('isLogined')).toBe(true);
-})
